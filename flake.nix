11201030aaaa535ccfaa693e860116d75a810edb
hkmangla <hemantmangla78@gmail.com>
2024-07-29 20:00:47 +0530

    nixified

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    common.url = "github:nammayatri/common";
+  };
+  outputs = inputs:
+    inputs.common.lib.mkFlake { inherit inputs; } {
+      debug = true;
+      perSystem = { self', pkgs, lib, config, ... }: {
+        haskellProjects.default = {
+          imports = [
+          ];
+          autoWire = [ "packages" "checks" ];
+        };
+        process-compose = { };
+        packages.default = self'.packages.namma-dsl;
+        devShells.default = pkgs.mkShell {
+          # cf. https://haskell.flake.page/devshell#composing-devshells
+          inputsFrom = [
+            config.haskellProjects.default.outputs.devShell
+            config.pre-commit.devShell
+            config.flake-root.devShell
+          ];
+        };
+      };
+    };
+}
